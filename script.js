b54d0d60e299f5eb142eba2799b8e1fcca856ae8
Kataev Evgen <vinegarjack2012@gmail.com>
2019-12-04 21:27:26 +0200

    second

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
 (function (){
     "use strict";
+    const bodyTable = document.getElementById('data-table'); 
+    const sortTable = document.getElementById('table-head');
     async  function dataFrom() {        
         const response = await fetch('https://jsonplaceholder.typicode.com/users');
         const userInfo = await response.json();
-        //console.log(userInfo);
-        
-        const bodyTable = document.getElementById('data-table');
         bodyTable.innerHTML = userInfo.reduce((html, user) => 
                         html + `<tr id="${user.id}">
                                 <td> ${user.name}</td>
                                 <td> ${user.username}</td>
                                 <td> ${user.email}</td>
                                 <td> ${user.website}</td>
-                            </tr>`,'');
-    
-       
+                            </tr>`,'');  
     }   
 
     dataFrom();
-     
-    const sortTable = document.getElementsByTagName('thead');
-
+    
+    console.log(sortTable);
     sortTable.addEventListener('click', sortByField);
 
     function sortByField(event){
