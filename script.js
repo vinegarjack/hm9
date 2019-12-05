ad6baa0c1fedabfd18604a74f2301636ef7d6d10
Kataev Evgen <vinegarjack2012@gmail.com>
2019-12-05 20:41:37 +0200

    sort by field work now

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 (function (){
     "use strict";
+
     const bodyTable = document.getElementById('data-table'); 
     const sortTable = document.getElementById('table-head');
-    async  function dataFrom() {        
+    
+    async  function dataFrom(sortDefault = 'id') {        
         const response = await fetch('https://jsonplaceholder.typicode.com/users');
         const userInfo = await response.json();
-        bodyTable.innerHTML = userInfo.reduce((html, user) => 
+        const arrUserInfo = Array.from(userInfo).sort( (a,b)=>{ // sort array from json by parametr
+            if (a[sortDefault] > b[sortDefault]) {return 1}
+            if (a[sortDefault] < b[sortDefault]) {return -1}
+            return 0;
+        });
+
+        bodyTable.innerHTML = arrUserInfo.reduce((html, user) => 
                         html + `<tr id="${user.id}">
                                 <td> ${user.name}</td>
                                 <td> ${user.username}</td>
@@ -16,12 +24,13 @@
 
     dataFrom();
     
-    console.log(sortTable);
+    //console.log(sortTable);
     sortTable.addEventListener('click', sortByField);
 
     function sortByField(event){
         event.preventDefault();
         const sortField = event.target.getAttribute('class');
-        console.log(sortField);
+        //console.log(sortField);
+        dataFrom(sortField);
     }
 })();
