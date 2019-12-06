97262ac7ceb2fa8625bc61e5a75b52bfa0cb4283
Kataev Evgen <vinegarjack2012@gmail.com>
2019-12-06 02:02:56 +0200

    alfa version

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
     const bodyTable = document.getElementById('data-table'); 
     const sortTable = document.getElementById('table-head');
     const modalTarget = document.querySelector('.modal');
+    const modalClose = document.querySelector('.modal');
 
     //console.log(modalTarget);
 
@@ -30,7 +31,10 @@
     //console.log(sortTable);
     sortTable.addEventListener('click', sortByField);
     bodyTable.addEventListener('click', showModalWindow);
-
+    modalClose.addEventListener('click',() =>{
+        modalTarget.style.opacity = 0;
+        modalTarget.style.zIndex = 10;
+    })
 
     function sortByField(event){
         event.preventDefault();
@@ -45,10 +49,30 @@
         const userId = event.target.parentNode.getAttribute('id');
         const response = await fetch('https://jsonplaceholder.typicode.com/users');
         const userInfo = await response.json();
-        
         const arrUserInfo = Array.from(userInfo).filter(user => user.id == userId); // как сделать чтобы сравниение было через ===
-        console.log(arrUserInfo);
-       
+        const modalUserInfo = arrUserInfo[0];
+        console.log(modalUserInfo);
+        modalTarget.innerHTML = arrUserInfo.reduce((html, user) => 
+                        html + `<p> Name : ${user.name} </p>
+                                <p> Username : ${user.username} </p>
+                                <p> Email : ${user.email} </p>
+                                <p> Website : ${user.website} </p>
+                                <p> Phone : ${user.phone} </p>
+                                <p> Address:</p> 
+                                <ul><li> City: ${user.address.city} </li>
+                                <li> street: ${user.address.street} </li>
+                                <li> suite: ${user.address.suite} </li>
+                                <li> geo: lat :${user.address.geo.lat}, lng: ${user.address.geo.lng}</li>
+                                <li> zipcode: ${user.address.zipcode} </li></ul>
+                                <p> Company:</p>
+                                <ul><li> name: ${user.company.name} </li>
+                                <li> bs: ${user.company.bs} </li>
+                                <li> catchPhrase: ${user.company.catchPhrase} </li></ul>
+                                `,'');
+                            
+        modalTarget.style.opacity = 1;
+        modalTarget.style.zIndex = 30;
     }
 
+    
 })();
